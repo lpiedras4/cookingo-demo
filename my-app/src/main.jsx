458df1cd2ad25efe1a1db9b3b56c483f4eca6e84
lpiedras4 <piedras.leo@gmail.com>
2026-04-28 11:53:01 -0700

    Started implementring router dom

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Navbar from './components/Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
