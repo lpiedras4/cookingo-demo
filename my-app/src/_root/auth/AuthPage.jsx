bfa0a80751fe35867711e2349c561df0d70f501b
lpiedras4 <piedras.leo@gmail.com>
2026-05-27 14:27:25 -0700

    Se agregó componente de login

--- my-app/src/_root/auth/AuthPage.jsx
+++ my-app/src/_root/auth/AuthPage.jsx
@@ -3,11 +3,14 @@ import SignInForm from "./forms/SignInForm";
 
 const AuthPage = () => {
   return (
-    <div className="mx-auto max-w-4xl px-5 py-6 md:px-8">
-      <h1 className="font-display text-3xl font-bold text-stone-800">
+    <div className=" flex flex-col items-center min-h-screen w-full bg-forest px-5 py-6 md:px-8 ">
+      <h1 className="font-display text-3xl font-bold text-white my-10">
         Aprende a cocinar con Cookin<span className="text-orange">Go</span>
       </h1>
-      <SignInForm />
+      <div className="flex flex-col items-center justify-center">
+        <SignInForm />
+      </div>
+      
     </div>
   );
 };
